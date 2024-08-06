f059ed6a563d0932c038a972ee1c85b8b96ebb6b
murodjon72 <muro21601@gmail.com>
2024-08-06 13:45:21 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,73 @@
+//age = +prompt('введите ваш возраст');
+//
+//
+//function sortAge(age){
+//if(age => 18){
+//    alert('заходите')
+//    if (age < 18 ){
+//        alert('не зайти')
+//       }
+//       else{
+//        alert('Дебил')
+//    }
+//}
+//}
+//
+//sortAge(age)
+//
+//const a = +prompt('введите первое число')
+//const b = +prompt('введите второе число ')
+//const operation = prompt('какое операцию вы хотите выполнить')
+//let result ;
+//
+//function calculator(operation){
+//    if(operation === '+'){
+//      result = a + b
+//
+//    }
+//
+//
+// else   if(operation === '-'){
+//         result = a - b
+//   
+//       }
+//
+//      else  if(operation === '*'){
+//         result = a * b
+//   
+//       }
+//
+//      else if(operation === '%'){
+//         result = a % b
+//   
+//       }
+//
+//      else if(operation === '**'){
+//         result = a ** b
+//   
+//       }
+//
+//
+//       if(operation === '/'){
+//         result = a / b
+//        
+//        if(b === 0){
+//             result = 'ошибка'
+//        }
+//       }
+//
+//       console.log(result)
+//}
+//
+//calculator(operation)
+
+
+let arr = ('Alex', 'George', 'Michael')
+
+function lenght(arr){
+if(arr.lenght  = 5 ) {
+    console.log(arr);
+}
+}
+
+lenght(arr)
